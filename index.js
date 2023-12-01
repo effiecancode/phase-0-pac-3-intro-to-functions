aa7fc7b3fd264fbb50344d8fff3a226c24f97970
effiecancode <faitheffie25@gmail.com>
2023-12-02 02:05:56 +0300

    return values

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // Follow along with the examples here
 
-function sayHello() {
-    console.log('Hello')
+// function sayHelloTo(firstName) {
+//     console.log(`Hello, ${firstName}!`);
+//   }
+
+//   sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+//   sayHelloTo("Jane"); // "Hello, Jane!"
+//   sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+//   sayHelloTo(1); // "Hello, 1!"
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+
+// say('Hello', 'effie');
+
+function add(x,y) {
+    console.log('results')
+    return x+y;
 }
 
-sayHello();
+console.log(add(6,8));
